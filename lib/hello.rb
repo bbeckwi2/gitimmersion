ec63d6ac4208a345a29ee24c32e6ff4c58d6b630
Brandon Beckwith <bbeckwi2@uncc.edu>
2016-12-02 18:45:11 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Jim Weirich
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
